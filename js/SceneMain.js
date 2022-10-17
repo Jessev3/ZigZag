635df4edb2ef57e9b77d1069188d73a285fe0033
Jessev3 <jesse.vugteveen@student.nhlstenden.com>
2022-10-17 23:26:39 +0200

    fixed velocity:

--- js/SceneMain.js
+++ js/SceneMain.js
@@ -12,10 +12,7 @@ class MainScene extends Phaser.Scene {
 
     create ()
     {
-        // var ReallyLargeWidth = 600;
-        // var ReallyLargeHeight = 1000;
-
-        // this.game.world.setBounds(width=600, height=10000);
+        
         this.xVelocity = 1;
         this.yVelocity = 0.6;
 
@@ -28,14 +25,14 @@ class MainScene extends Phaser.Scene {
         this.ball = this.physics.add.sprite(275, 525, 'ball');
         this.ball.scale = 0.2;
         this.ball.depth = this.depth;
-
+        
         this.startingPlatform = this.add.isobox(this.x, this.y, this.width, this.height, 0x00b9f2, 0x016fce, 0x028fdf);
         this.startingPlatform.depth = this.depth - 1;
 
         this.platforms = this.physics.add.staticGroup();
 
         // this.cameras.main.setBounds(0, 0, 600, 1000);
-        this.cameraSpeed = 1;
+        this.cameraSpeed = 0.6;
         this.cameras.main.setViewport(0, 0, 600, 0);
         
         this.createPlatforms(20);
@@ -49,22 +46,17 @@ class MainScene extends Phaser.Scene {
 
 
         this.spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-        // this.input.keyboard.on('keydown-SPACE', () => {
-            // this.xVelocity = 1 ? this.xVelocity = -1 : this.xVelocity = 1; 
-        // }, this)
+
+        this.input.keyboard.on('keydown-SPACE', () => {
+            this.xVelocity == 1 ? this.xVelocity = -1 : this.xVelocity = 1; 
+        }, this)
 
 
-        // this.input.keyboard.on('keydown-SPACE', this.check, this);
     }
 
     update() {
         // Move the camera 1 pixel at a time so that the screen moves vertically
-        // this.cameras.main.y += this.cameraSpeed;
-        
-        if(this.spaceBar.isDown) {
-            this.xVelocity === 1 ? this.xVelocity = -1 : this.xVelocity = 1; 
-        }
-
+        this.cameras.main.y += this.cameraSpeed;
         this.ball.x += this.xVelocity;
         this.ball.y -= this.yVelocity;
        
