25c259b5d3a98fa60523fb893c58604f4d877eac
Jessev3 <jesse.vugteveen@student.nhlstenden.com>
2022-10-18 19:14:32 +0200

    tried some things

--- js/SceneMain.js
+++ js/SceneMain.js
@@ -12,7 +12,7 @@ class MainScene extends Phaser.Scene {
 
     create ()
     {
-        
+        this.test = true;
         this.xVelocity = 1;
         this.yVelocity = 0.6;
 
@@ -24,26 +24,35 @@ class MainScene extends Phaser.Scene {
 
         this.ball = this.physics.add.sprite(275, 525, 'ball');
         this.ball.scale = 0.2;
-        this.ball.depth = this.depth;
+        this.ball.depth = this.depth; // change to this.depth
+
         
         this.startingPlatform = this.add.isobox(this.x, this.y, this.width, this.height, 0x00b9f2, 0x016fce, 0x028fdf);
         this.startingPlatform.depth = this.depth - 1;
-
+        this.startingPlatform.enable = true;
         this.platforms = this.physics.add.staticGroup();
+        
+        // this.startingRectangle = this.add.rectangle(this.x, this.y - 120, 100, 100, 0x016fce);
+        // this.startingRectangle.depth = 1001;
+        // this.startingRectangle.angle = 45;
+        // this.startingRectangle.scaleX = 0.8;
+        // this.startingRectangle.scaleY = 0.8;
+        // this.rectangles = this.physics.add.staticGroup();
 
-        // this.cameras.main.setBounds(0, 0, 600, 1000);
+
         this.cameraSpeed = 0.6;
         this.cameras.main.setViewport(0, 0, 600, 0);
         
-        this.createPlatforms(20);
+        this.createPlatforms(20); //aanpassen
 
         this.timer = this.time.addEvent({
-            delay: 2000 / this.cameraSpeed,
+            delay: 2000 / this.cameraSpeed, //aanpassen
             callback: this.createPlatforms,
             callbackScope: this,
             loop: true
         })
 
+        
 
         this.spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
@@ -51,15 +60,26 @@ class MainScene extends Phaser.Scene {
             this.xVelocity == 1 ? this.xVelocity = -1 : this.xVelocity = 1; 
         }, this)
 
+       
+        console.log(this.ball.body.touching);
 
     }
 
     update() {
-        // Move the camera 1 pixel at a time so that the screen moves vertically
         this.cameras.main.y += this.cameraSpeed;
         this.ball.x += this.xVelocity;
         this.ball.y -= this.yVelocity;
-       
+
+        // overlap or collide   
+        this.physics.add.collider(
+            this.ball,
+            this.platforms,
+            this.check,
+            null,
+            this
+          );
+
+        
     }
 
     createPlatforms(platformLength = 15) {
@@ -72,9 +92,13 @@ class MainScene extends Phaser.Scene {
                     }
                     this.x += this.width / 2;
                     this.y -= this.width / 4;
+
                     var platform = this.add.isobox(this.x, this.y, 120, 130, 0x00b9f2, 0x016fce, 0x028fdf);
                     platform.depth = this.depth -= 1;
                     platform.showLeft = false;
+                    // platform.showLeft = false;
+                    // platform.showRight = false;
+
                     this.platforms.add(platform);
                 }
             } else {
@@ -84,10 +108,13 @@ class MainScene extends Phaser.Scene {
                     }
                     this.x -= this.width / 2;
                     this.y -= this.width / 4;
+
                     var platform = this.add.isobox(this.x, this.y, 120, 130, 0x00b9f2, 0x016fce, 0x028fdf);
                     platform.depth = this.depth -= 1;
                     platform.showLeft = true;
                     platform.showRight = false;
+                    // platform.showLeft = false;
+                    // platform.showRight = false;
                     this.platforms.add(platform);
                 }
             }
@@ -95,6 +122,10 @@ class MainScene extends Phaser.Scene {
     }
 
     check() {
-        this.xVelocity = 1 ? this.xVelocity = -1 : this.xVelocity = 1; 
+        
+        // console.log(this.ball.body.touching);
+        
     }
+
+
 }
