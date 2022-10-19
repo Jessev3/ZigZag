6c678e58230f524c20d6289c506168c880dc6c97
Jessev3 <jesse.vugteveen@student.nhlstenden.com>
2022-10-19 20:16:13 +0200

    changed method to polygons

--- js/SceneMain.js
+++ js/SceneMain.js
@@ -12,35 +12,58 @@ class MainScene extends Phaser.Scene {
 
     create ()
     {
-        this.offsetLeft = 710;
-        this.offsetRight = 410;
+        
 
-        this.xVelocity = 1;
-        this.yVelocity = 0.5;
-
+        // Set all vars for creating platforms
         this.x = 300;
         this.y = 650;
         this.width = 120;
         this.height = 60;
         this.depth = 1000;
 
+        // Create ball sprite
         this.ball = this.physics.add.sprite(300, 590, 'ball');
         this.ball.scale = 0.05;
-        this.ball.depth = this.depth;  
+        this.ball.depth = 10000;  
+
+        // set speed of ball
+        this.xVelocity = 1;
+        this.yVelocity = 0.5;
 
         
+        // create starting platform. Which is always the same
         this.startingPlatform = this.add.isobox(this.x, this.y, this.width, this.height, 0x00b9f2, 0x016fce, 0x028fdf);
         this.startingPlatform.depth = this.depth - 1;
         this.startingPlatform.enable = true;
 
+        // Make group to put all platforms in
         this.platforms = this.physics.add.staticGroup();
         this.platforms.add(this.startingPlatform);
         this.platformWithBall = this.startingPlatform;
 
+
+        // create starting polygon to detect if ball is inside or outside the isobox
+        this.graphics = this.add.graphics();
+        this.polygons = []
+        this.startingPolygon = new Phaser.Geom.Polygon([
+            this.startingPlatform.getTopCenter().x - 60, this.startingPlatform.getTopCenter().y - 30,
+            this.startingPlatform.getTopCenter().x, this.startingPlatform.getTopCenter().y - 60,
+            this.startingPlatform.getTopCenter().x + 60, this.startingPlatform.getTopCenter().y - 30,
+            this.startingPlatform.getTopCenter().x, this.startingPlatform.getTopCenter().y
+        ]);
+        this.polygons.push(this.startingPolygon);
+        this.graphics.fillStyle(0x00aa00);
+        this.graphics.fillPoints(this.startingPolygon.points, true); 
+        this.graphics.depth = 1000;
+
+
+        // set camera settings
         this.cameraSpeed = 0.6;
         this.cameras.main.setViewport(0, 0, 600, 0);
         
-        this.createPlatforms(2); //aanpassen
+
+        // create first platforms, then every 2 seconds 15 platforms
+        this.createPlatforms(20); 
 
         // this.timer = this.time.addEvent({
         //     delay: 2000 / this.cameraSpeed, //aanpassen
@@ -56,54 +79,21 @@ class MainScene extends Phaser.Scene {
         this.input.keyboard.on('keydown-SPACE', () => {
             this.xVelocity == 1 ? this.xVelocity = -1 : this.xVelocity = 1; 
         }, this)
-    
 
     }
 
     update() {
         // this.cameras.main.y += this.cameraSpeed;
-        this.ball.x += this.xVelocity;
-        this.ball.y -= this.yVelocity;
+        // this.ball.x += this.xVelocity;
+        // this.ball.y -= this.yVelocity;
 
         // overlap or collide   
 
-        this.platforms.children.each((platform) => {
-            if(this.ball.x > platform.getTopCenter().x -60 && this.ball.x < platform.getTopCenter().x + 60) {
-                if(this.ball.y < platform.getTopCenter().y && this.ball.y > platform.getTopCenter().y - 60) {
-                    this.platformWithBall = platform;
-                }
-            }
-            
-            // var leftLine = -0.5 * this.ball.x + this.offsetLeft;
-            // var rightLine = 0.5 * this.ball.x + this.offsetRight;
-            // this.offsetLeft -= 60;
-            // this.offsetRight -= 60; 
-
-            // if(this.ball.y >= rightLine || this.ball.y >= leftLine) {
-            //     this.platformWithBall = platform;
-            //     console.log(this.platformWithBall.getData("test"));
-            // }
-
-            
+        this.polygons.forEach((polygon) => {
+            if(Phaser.Geom.Polygon.Contains(polygon, this.ball.x, this.ball.y)) {
+                
+            } 
         })
-
-    
-        if(this.ball.x > this.platformWithBall.getTopCenter().x + 60 && this.ball.y < this.platformWithBall.getTopCenter().y - 30 || this.ball.y < this.platformWithBall.getTopCenter().y -60) {
-            this.ball.body.gravity.y = 5000;
-            this.ball.depth = 0;
-        }
-
-        if(this.ball.x < this.platformWithBall.getTopCenter().x - 60 && this.ball.y < this.platformWithBall.getTopCenter().y - 30 || this.ball.y < this.platformWithBall.getTopCenter().y -60) {
-            this.ball.body.gravity.y = 5000;
-            this.ball.depth = 0;
-        }
-
-        // if(this.ball.y < this.platformWithBall.data.values.rightLine || this.ball.y < this.platformWithBall.data.values.leftLine) {
-        //     this.ball.body.gravity.y = 5000;
-        //     this.ball.depth = 0;
-        // }
-
-        // switch case ? die kijkt welke kan geen aanliggend platform heeft 
     }
 
     createPlatforms(platformLength = 15) {
@@ -121,8 +111,8 @@ class MainScene extends Phaser.Scene {
                     var platform = this.add.isobox(this.x, this.y, this.width, this.height, 0x00b9f2, 0x016fce, 0x028fdf);
                     platform.depth = this.depth -= 1;
                     platform.showLeft = false;
-
                     this.platforms.add(platform);
+                    this.createPolygon(platform);
                 }
             } else {
                 for(var left = 1; left <= randomInt; left++) {
@@ -136,13 +126,24 @@ class MainScene extends Phaser.Scene {
                     platform.depth = this.depth -= 1;
                     platform.showLeft = true;
                     platform.showRight = false;
-
                     this.platforms.add(platform);
+                    this.createPolygon(platform);
                 }
             }
         }
     }
 
-
+    createPolygon(platform) {
+        var polygon = new Phaser.Geom.Polygon([
+            platform.getTopCenter().x - 60, platform.getTopCenter().y - 30,
+            platform.getTopCenter().x, platform.getTopCenter().y - 60,
+            platform.getTopCenter().x + 60, platform.getTopCenter().y - 30,
+            platform.getTopCenter().x, platform.getTopCenter().y
+        ]);
+        this.graphics.fillStyle(0x00aa00);
+        this.graphics.fillPoints(polygon.points, true); 
+        this.graphics.depth = 1000;
+        this.polygons.push(polygon);
+    }
 
 }
